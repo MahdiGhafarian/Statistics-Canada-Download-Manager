a609eb1c8dc9556894857536c20a86fe1652f9b7
Mahdi Ghafarian <mahdi.ghaf@outlook.com>
2025-01-27 21:41:48 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -9,17 +9,6 @@ Date: 2025-01-26
 # Initialization
 # -------------------------------------------------------------------------
 
-import subprocess
-import sys
-
-# Function to install missing modules
-def install_missing_modules(modules):
-    for module in modules:
-        try:
-            __import__(module)
-        except ImportError:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", module])
-
 # List of required modules
 required_modules = ['tkinter', 'requests']
 
